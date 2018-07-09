658cac7842a3a1528e9a10609d32f994324289b1
Jeff Vitunac <ejv20@pitt.edu>
2018-07-09 14:50:46 -0400

    Adding Posts
    Signed-off-by: Jeff Vitunac <ejv20@pitt.edu>

--- daser.py
+++ daser.py
@@ -1,7 +1,7 @@
+from datetime import datetime
+
 from flask import Flask, request, url_for, redirect, session, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
 
 app = Flask(__name__, template_folder="FrontEnd/render_templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
@@ -12,13 +12,31 @@ app.config.update(dict(SEND_FILE_MAX_AGE_DEFAULT=0))
 users = {}
 
 
-class User(db.Model):
-    name = db.Column(db.String(80), primary_key=True, unique=True)
-    pas = db.Column(db.String(80))
+class Post(db.Model):
+    __tablename__ = "posts"
+    id = db.Column('post_id', db.Integer, primary_key=True, unique=True)
+    text = db.Column('post_text', db.String(300))
+    time = db.Column('post_time', db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    user = db.relationship('User')
 
-    def __init__(self, nm, ps):
+    def __init__(self, txt, usr, tm):
+        self.text = txt
+        self.user_id = usr
+        self.time = tm
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True, unique=True)
+    name = db.Column('user_name', db.String(80), unique=True)
+    pas = db.Column('user_pw', db.String(80))
+    date = db.Column('created_date', db.String(20))
+
+    def __init__(self, nm, ps, tm):
         self.name = nm
         self.pas = ps
+        self.created = tm
 
 
 # by default, direct to login
@@ -39,7 +57,7 @@ def logger():
     # first check if the user is already logged in
     if "username" in session and "username" in users:
         if session["username"] in users:
-            return redirect(url_for("start", username=session["username"]))
+            return redirect(url_for("feed", username=session["username"]))
 
     # if not, and the incoming request is via POST try to log them in
     elif request.method == "POST":
@@ -47,28 +65,29 @@ def logger():
             if request.form["user"] in users and users[request.form["user"]] == request.form["pass"]:
                 session["username"] = request.form["user"]
                 session_name = request.form["user"]
-                return redirect(url_for("start", username=session_name))
+                return redirect(url_for("feed", username=session_name))
             else:
                 login_error = 'Incorrect Username or Password'
         elif request.form["but"] == "Sign Up":
             if request.form["nuser"] not in users:
-                u5 = User(request.form["nuser"], request.form["npass"])
+                u5 = User(request.form["nuser"], request.form["npass"], datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                 db.session.add(u5)
                 db.session.commit()
 
                 session["username"] = request.form["nuser"]
                 session_name = request.form["nuser"]
-                return redirect(url_for("start", username=session_name))
+                return redirect(url_for("feed", username=session_name))
             else:
                 login_error = 'Name already taken'
 
-    return render_template("login.html", error=login_error)
+    return render_template('login.html', error=login_error)
 
 
-@app.route("/start")
-def start():
+@app.route("/feed")
+def feed():
+
     # Render base template
-    return render_template("index.html", name=session['username'])
+    return render_template("feed.html", name=session['username'])
 
 
 # initialize the database
@@ -78,16 +97,24 @@ def initdb_command():
     db.drop_all()
     db.create_all()
 
-    u1 = User("Je", "Pass")
-    u2 = User("Ni", "Pass")
-    u3 = User("Ga", "Pass")
-    u4 = User("Em", "Pass")
+    u1 = User("Je", "Pass", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    u2 = User("Ni", "Pass", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    u3 = User("Ga", "Pass", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    u4 = User("Em", "Pass", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+    p1 = Post("Hello World", "Je", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    p2 = Post("Hello World - Nick", "Ni", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    p3 = Post("Hello World - Gabe", "Ga", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    p4 = Post("Hello World - Emily", "Em", datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
     db.session.add(u1)
     db.session.add(u2)
     db.session.add(u3)
     db.session.add(u4)
-
+    db.session.add(p1)
+    db.session.add(p2)
+    db.session.add(p3)
+    db.session.add(p4)
     # commit
     db.session.commit()
     print('Initialized the database.')
